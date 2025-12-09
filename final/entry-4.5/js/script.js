9ed6a3fbdd1bbc065c14cdb5e84ec833126df629
Mappel00 <mappel0304@gmail.com>
2025-12-09 14:24:23 -0600

    update

--- final/entry-4.5/js/script.js
+++ final/entry-4.5/js/script.js
@@ -1,6 +1,6 @@
 
-$("h1").on("click", function(){
-    $("h1").css({"color":"red"})
-});
- $("h1").on("click", function(){
-  $(this).addClass("color");});
+$("#main").hide();
+
+$("#button").on("click", function(){
+   $("#main").show();
+})
